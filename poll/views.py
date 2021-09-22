3c5906857eb1c3738ffafc7e73dfde34d90d7728
Shmyaks <maxonic10@list.ru>
2021-09-22 16:46:58 +0300

    Initial views

---
+++ poll/views.py
@@ -0,0 +1,63 @@
+import re
+from .models import Answer, Poll, Question, Report
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import *
+from rest_framework.generics import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+# Create your views here.
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    
+
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ReportCreate(generics.CreateAPIView):
+    serializer_class = ReportStartSerializer
+
+
+    def perform_create(self, serializer):
+        report = serializer.save()
+        questions = Question.objects.filter(poll = report.poll_id).all()
+        [(Answer(ans = "", question_id = question, report_id = report).save()) 
+            for question in questions]
+
+
+class ReportStop(generics.UpdateAPIView):
+    queryset = Report.objects.all()
+    serializer_class = ReportStopSerializer
+
+
+class AnswerSend(generics.UpdateAPIView):
+    queryset = Report.objects.all()
+    serializer_class = AnswersSendSerializer
+
+    def perform_update(self, serializer):
+        print(serializer.data)
+
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionlSerializer
+      
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionlSerializer
+
+
+class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer 
+
+
+class UserReportsDetail(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = ReportSerializer 
